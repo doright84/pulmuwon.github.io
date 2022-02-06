7cb741e69d53d1e941ca5de8637c3b5a6a1e4a94
Kim Do Hee <95347166+doright84@users.noreply.github.com>
2022-02-07 03:35:56 +0900

    풀무원 완

--- js/script.js
+++ js/script.js
@@ -1,47 +1,73 @@
-$(document).ready(function() {
-    window.onload = function() {
-        // swiper 슬라이드 관련
-        new Swiper('.slide-main', {
+$(document).ready(function () {
+    // gotop---------
+    $(".gotop").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 80) { // 400이 넘어가면 고 탑이 슬라이드 다운으로 보이고 아니면 다시 fade out으로 사라짐.
+            $('.gotop').fadeIn();
+        } else {
+            $('.gotop').fadeOut();
+        }
+    });
+    // swiper 슬라이드 관련
+    new Swiper('.slide-main', {
 
-            // 슬라이드 반복
-            loop: true,
-            autoplay: {
-                delay: 4000,
-                disableOnInteraction: false,
-            },
-            effect: 'fade',
-            fadeEffect: {
-                crossFade: true,
-                speed: 5000,
-            },
-            //로그인 슬라이드
+        // 슬라이드 반복
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+            speed: 5000,
+        },
+       
+        //로그인 슬라이드
+    });
+    new Swiper('.log-sw', {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        autoplay: {
+            delay: 4000,
+            // disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.log-pg',
+            clickable: true,
+        },
+    });
+    new Swiper('.free-sw-main', {
+
+        // 슬라이드 반복
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+            speed: 8000,
+        },
+        //로그인 슬라이드
+    });
+
+    // 베스트제품 오렌지박스
+    let box = $('.orange-box');
+    var menu = $('.best-list li');
+    var menu_txt = $('.best-list>li>a>p');
+    $.each(menu, function (index, item) {
+        $(this).click(function () {
+            box.css({
+                top: index * 55
+            });
         });
-        new Swiper('.log-sw', {
-            slidesPerView: 4,
-            // 슬라이드 반복
-           
-            autoplay: {
-                delay: 4000,
-                // disableOnInteraction: false,
-            },
 
-
-        });
-        new Swiper('.free-sw-main', {
-
-            // 슬라이드 반복
-            loop: true,
-            autoplay: {
-                delay: 4000,
-                disableOnInteraction: false,
-            },
-            effect: 'fade',
-            fadeEffect: {
-                crossFade: true,
-                speed: 8000,
-            },
-            //로그인 슬라이드
-        });
-    }
+    });
+    menu.click(function () {
+        $(this).find(menu_txt).addClass('text-on')
+        $(this).siblings().find(menu_txt).removeClass('text-on');
+    });
 
 });
